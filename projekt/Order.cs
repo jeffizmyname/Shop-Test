bf2eda1f9c71fe3522b9ed9b6e19e3835150cc47
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-06-09 02:51:32 +0200

    order class handles order stuff

---
+++ projekt/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt
+{
+    public class Order
+    {
+        public int userID { get; set; }
+        public bool FastDelivery { get; set; } = false;
+        public string? deliveryAddres { get; set; }
+        public List<Product> products { get; set; }
+
+        public Order() { }
+        public void Initialize(int userID, List<Product> products) { 
+            this.userID = userID; 
+            this.products = products;
+        }
+    }
+}
