045a31b86a5ee70fdc14fe0120801d6b2704fc0a
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-06-09 19:07:39 +0200

    registre working you can register owowowowowo

--- projekt/register.xaml.cs
+++ projekt/register.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Org.BouncyCastle.Tls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,62 @@ namespace projekt
         public register()
         {
             InitializeComponent();
+        }
+
+        private void TopBorder_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string imie = userName.Text;
+            string nazwisko = userSurname.Text;
+            string login = userLogin.Text;
+            string pesel = userPesel.Text;
+            string haslo = userPass.Text;
+            String hash = PasswordHasher.Hash(haslo);
+
+            if (!string.IsNullOrEmpty(imie) && !string.IsNullOrEmpty(nazwisko) && !string.IsNullOrEmpty(login) && !string.IsNullOrEmpty(pesel) && !string.IsNullOrEmpty(haslo))
+            {
+                if (Conection.query($"SELECT * FROM users WHERE login = '{login}'") == "[]")
+                {
+                    if (Conection.query($"SELECT * FROM users WHERE pesel = '{pesel}'") == "[]")
+                    {
+                        Conection.query($"INSERT INTO users VALUES ('', '{imie}','{nazwisko}','{login}','{hash}','{pesel}', 'U')");
+                        login log = new login();
+                        log.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        // Błąd PESEL
+                        MessageBox.Show("PESEL już istnieje. Proszę wprowadzić inny PESEL.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    // Błąd loginu
+                    MessageBox.Show("Login już istnieje. Proszę wybrać inny login.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                // Błąd wypełnienia pól
+                MessageBox.Show("Wszystkie pola są wymagane. Proszę wypełnić wszystkie dane.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LoginRedirect_Click(object sender, RoutedEventArgs e)
+        {
+            login log = new login();
+            log.Show();
+            this.Close();
         }
     }
 }
